3a7f6aa26c828146edb512938b6125f9961b00c2
Alexandre Morais <alexandremorais26@gmail.com>
2022-10-11 08:47:43 -0300

    feat: notification modal

--- components/pages/Dashboard/TeamsList.tsx
+++ components/pages/Dashboard/TeamsList.tsx
@@ -1,5 +1,4 @@
-import { Button, Flex, Icon } from '@chakra-ui/react';
-import { AiOutlineRight } from 'react-icons/ai';
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import { TeamCard } from './Cards';
 
