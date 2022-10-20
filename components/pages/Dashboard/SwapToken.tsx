61c9478802ab29b062815a480737ce996048dd36
Alexandre Morais <alexandremorais26@gmail.com>
2022-10-20 19:03:47 -0300

    feat: add icon on create organization card

--- components/pages/Dashboard/SwapToken.tsx
+++ components/pages/Dashboard/SwapToken.tsx
@@ -5,7 +5,6 @@ import {
 	Icon,
 	Input,
 	InputGroup,
-	InputRightElement,
 	Menu,
 	MenuButton,
 	MenuItem,
@@ -46,7 +45,7 @@ export const SwapToken = () => {
 					{translate('title')}
 				</Text>
 			</Flex>
-			<Flex direction="column" align="center" gap="6" w="max-content">
+			<Flex direction="column" align="center" gap="6" w="max-content" px="6">
 				<Flex direction="column" gap="2" w="full">
 					<Text color="white" fontSize="sm">
 						{translate('youPay')}
