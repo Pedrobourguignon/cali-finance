6da95e17992ce605f3575d5fbe3ebf083aec7d78
Pedrobourguignon <pedrobourguignon@hotmail.com>
2022-11-21 18:16:34 -0300

    feat: implement network select

--- containers/organizations/create.tsx
+++ containers/organizations/create.tsx
@@ -2,8 +2,8 @@ import {
 	Button,
 	Flex,
 	FormControl,
+	Img,
 	Input,
-	Select,
 	Text,
 	Textarea,
 	TextProps,
@@ -11,24 +11,41 @@ import {
 import { NewOrganizationLinks, NavigationBack } from 'components';
 import { usePicasso } from 'hooks';
 import { AppLayout } from 'layouts';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { createOrganizationSchema, navigationPaths } from 'utils';
 import { INetwork } from 'types';
+import { Select } from 'chakra-react-select';
 
 interface ICreateOrganization {
 	name: string;
-	type: string;
+	type: { label: string; value: string };
 	email: string;
-	network: string;
+	network: { label: string; value: string; icon: string };
 	description?: string;
 }
 
-const organizationsType = ['DAO', 'financial', 'e-commerce'];
-const networksType: INetwork[] = [
-	{ name: 'Ethereum', icon: '/images/eth.png' },
-	{ name: 'Polygon', icon: '/images/polygon.png' },
-	{ name: 'BNB Chain', icon: '/images/bnbchain.png' },
+interface INetworkSelect {
+	value: string;
+	label: string;
+	icon: string;
+}
+
+interface IBasicSelect {
+	value: string;
+	label: string;
+}
+
+const organizationsType: IBasicSelect[] = [
+	{ value: 'DAO', label: 'DAO' },
+	{ value: 'financial', label: 'financial' },
+	{ value: 'e-commerce', label: 'e-commerce' },
+];
+
+const networksType: INetworkSelect[] = [
+	{ value: 'Ethereum', label: 'Ethereum', icon: '/images/eth.png' },
+	{ value: 'Polygon', label: 'Polygon', icon: '/images/polygon.png' },
+	{ value: 'BNB Chain', label: 'BNB Chain', icon: '/images/bnbchain.png' },
 ];
 
 const labelStyle: TextProps = {
@@ -40,8 +57,8 @@ const labelStyle: TextProps = {
 export const CreateOrganization = () => {
 	const theme = usePicasso();
 	const {
-		register,
 		handleSubmit,
+		control,
 		formState: { errors },
 	} = useForm<ICreateOrganization>({
 		resolver: yupResolver(createOrganizationSchema),
@@ -71,17 +88,26 @@ export const CreateOrganization = () => {
 								<Text color="black" fontSize="xl" fontWeight="medium">
 									Create Organization
 								</Text>
-								<Input
-									color="black"
-									placeholder="Organization Name *"
-									borderBottomWidth="0,125rem"
-									borderBottomColor="black"
-									borderRadius="none"
-									px="1"
-									fontSize="2xl"
-									_placeholder={{ color: 'blackAlpha.500', fontSize: '2xl' }}
-									_hover={{}}
-									{...register('name')}
+								<Controller
+									render={({ field }) => (
+										<Input
+											{...field}
+											color="black"
+											placeholder="Organization Name *"
+											borderBottomWidth="0,125rem"
+											borderBottomColor="black"
+											borderRadius="none"
+											px="1"
+											fontSize="2xl"
+											_placeholder={{
+												color: 'blackAlpha.500',
+												fontSize: '2xl',
+											}}
+											_hover={{}}
+										/>
+									)}
+									name="name"
+									control={control}
 								/>
 								<Text fontSize="xs" color="red" position="absolute" top="100%">
 									{errors.name?.message}
@@ -97,47 +123,48 @@ export const CreateOrganization = () => {
 									<Flex direction="column" color="black" gap="6">
 										<Flex direction="column">
 											<Text {...labelStyle}>Type *</Text>
-											<Select
-												borderColor={theme.bg.primary}
-												color="blackAlpha.500"
-												bgColor="white"
-												_hover={{}}
-												{...register('type')}
-											>
-												<option
-													selected
-													disabled
-													style={{
-														background: 'white',
-													}}
-												>
-													Please select an organization type
-												</option>
-												{organizationsType.map((item, index) => (
-													<option
-														key={+index}
-														style={{
-															background: 'white',
-															color: 'black',
+											<Controller
+												name="type"
+												control={control}
+												render={({ field }) => (
+													<Select
+														{...field}
+														chakraStyles={{
+															control: group => ({
+																...group,
+																bg: 'white',
+																minWidth: '48',
+																borderColor: '#121212',
+															}),
+															menuList: group => ({
+																...group,
+																bg: 'white',
+																borderColor: '#121212',
+															}),
 														}}
-													>
-														{item}
-													</option>
-												))}
-											</Select>
+														options={organizationsType}
+													/>
+												)}
+											/>
 											<Text fontSize="xs" color="red">
 												{errors.type?.message}
 											</Text>
 										</Flex>
 										<Flex direction="column" position="relative">
 											<Text {...labelStyle}>Corporative e-mail *</Text>
-											<Input
-												placeholder="example@organization.io"
-												_placeholder={{ color: 'blackAlpha.500' }}
-												bgColor="white"
-												_hover={{}}
-												borderColor={theme.bg.primary}
-												{...register('email')}
+											<Controller
+												render={({ field }) => (
+													<Input
+														{...field}
+														placeholder="example@organization.io"
+														_placeholder={{ color: 'blackAlpha.500' }}
+														bgColor="white"
+														_hover={{}}
+														borderColor={theme.bg.primary}
+													/>
+												)}
+												name="email"
+												control={control}
 											/>
 											<Text
 												fontSize="xs"
@@ -150,14 +177,20 @@ export const CreateOrganization = () => {
 										</Flex>
 										<Flex direction="column">
 											<Text {...labelStyle}>Description</Text>
-											<Textarea
-												borderColor={theme.bg.primary}
-												_placeholder={{ color: 'blackAlpha.500' }}
-												_hover={{}}
-												bgColor="white"
-												placeholder="Hello Org is an organization that..."
-												minH="110"
-												{...register('description')}
+											<Controller
+												render={({ field }) => (
+													<Textarea
+														{...field}
+														borderColor={theme.bg.primary}
+														_placeholder={{ color: 'blackAlpha.500' }}
+														_hover={{}}
+														bgColor="white"
+														placeholder="Hello Org is an organization that..."
+														minH="110"
+													/>
+												)}
+												name="description"
+												control={control}
 											/>
 										</Flex>
 									</Flex>
@@ -176,36 +209,39 @@ export const CreateOrganization = () => {
 										<Text>Create Organization</Text>
 									</Button>
 								</Flex>
-								<Flex direction="column" minW="44" color="black">
+								<Flex direction="column" minW="44" color={theme.text.primary}>
 									<Text {...labelStyle}>Network *</Text>
-									<Select
-										borderColor={theme.bg.primary}
-										color="blackAlpha.500"
-										bgColor="white"
-										_hover={{}}
-										{...register('network')}
-									>
-										<option
-											selected
-											disabled
-											style={{
-												background: 'white',
-											}}
-										>
-											Please select an network
-										</option>
-										{networksType.map((item, index) => (
-											<option
-												key={+index}
-												style={{
-													background: 'white',
-													color: 'black',
+									<Controller
+										name="network"
+										control={control}
+										render={({ field }) => (
+											<Select
+												{...field}
+												chakraStyles={{
+													control: group => ({
+														...group,
+														bg: 'white',
+														minWidth: '48',
+														borderColor: '#121212',
+													}),
+													menuList: group => ({
+														...group,
+														bg: 'white',
+														borderColor: '#121212',
+													}),
 												}}
-											>
-												{item.name}
-											</option>
-										))}
-									</Select>
+												placeholder="select"
+												options={networksType}
+												// eslint-disable-next-line react/no-unstable-nested-components
+												formatOptionLabel={network => (
+													<Flex gap="2" align="center">
+														<Img src={network.icon} boxSize="5" />
+														<Text>{network.label}</Text>
+													</Flex>
+												)}
+											/>
+										)}
+									/>
 									<Text fontSize="xs" color="red">
 										{errors.type?.message}
 									</Text>
