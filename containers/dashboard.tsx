cb3730fccc3b4819b02206abfb91a0888f6010ac
Pedrobourguignon <pedrobourguignon@hotmail.com>
2022-12-07 14:39:13 -0300

    feat: implement app layout in dashboard page

--- containers/dashboard.tsx
+++ containers/dashboard.tsx
@@ -1,7 +1,12 @@
 import { Flex } from '@chakra-ui/react';
-import { Sidebar, DashboardComponent, MobileHeader } from 'components';
+import {
+	DashboardComponent,
+	DashboardRightBar,
+	MobileHeader,
+} from 'components';
 import { OrganizationsProvider } from 'contexts';
 import { usePicasso } from 'hooks';
+import { AppLayout } from 'layouts';
 
 import React from 'react';
 
@@ -9,17 +14,12 @@ export const DashboardContainer = () => {
 	const theme = usePicasso();
 	return (
 		<OrganizationsProvider>
-			<Flex
-				bg={theme.bg.primary}
-				minH="100vh"
-				h="full"
-				direction={{ base: 'column', sm: 'row' }}
-				py="6"
-			>
-				<MobileHeader />
-				<Sidebar />
-				<DashboardComponent />
-			</Flex>
+			<AppLayout right={<DashboardRightBar />}>
+				<Flex h="full" direction={{ base: 'column', sm: 'row' }} py="6">
+					<MobileHeader />
+					<DashboardComponent />
+				</Flex>
+			</AppLayout>
 		</OrganizationsProvider>
 	);
 };
