d6f40101b277aafff874439d40e1c02ac55ab0ed
Pedrobourguignon <pedrobourguignon@hotmail.com>
2022-12-08 15:35:26 -0300

    feat: handle button color and refactor design spacement

--- components/pages/app/RightBar/WithdrawsBanner/WithdrawsBar/WithdrawsBar.tsx
+++ components/pages/app/RightBar/WithdrawsBanner/WithdrawsBar/WithdrawsBar.tsx
@@ -1,41 +1,83 @@
 import { Button, Flex, Text } from '@chakra-ui/react';
 import { CircularProgressBar } from 'components';
+import { usePicasso } from 'hooks';
+import { useState } from 'react';
+
+const buttonOptions = ['This Week', 'This Month'];
 
 const withdrawnPercentage = 50;
-export const WithdrawsBar = () => (
-	<Flex direction="column" position="relative">
-		<Flex
-			w="100%"
-			px="4"
-			py="2.5"
-			bg="white"
-			borderRadius="base"
-			h="100%"
-			direction="column"
-			gap="3"
-		>
-			<Text color="black" fontWeight="medium" fontSize="md">
-				Withdrawals
-			</Text>
-			<CircularProgressBar percentage={withdrawnPercentage} />
-			<Flex zIndex="docked">
-				<Button color="black" bgColor="gray.100" borderRadius="full">
-					This Week
-				</Button>
-				<Button color="black">This Month</Button>
+export const WithdrawsBar = () => {
+	const theme = usePicasso();
+	const [selectedOption, setSelectedOption] = useState<string | undefined>(
+		'This Week'
+	);
+
+	const handleSelectedOption = (btnName: string) => {
+		const filter = buttonOptions.find(item => item === btnName);
+		setSelectedOption(filter);
+	};
+
+	return (
+		<Flex direction="column" position="relative">
+			<Flex
+				w="60"
+				px="4"
+				py="2.5"
+				bg="white"
+				borderRadius="base"
+				h="100%"
+				direction="column"
+				gap="3"
+			>
+				<Text color="black" fontWeight="medium" fontSize="md">
+					Withdrawals
+				</Text>
+				<CircularProgressBar percentage={withdrawnPercentage} />
+				<Flex zIndex="docked" justify="center">
+					{buttonOptions.map((item, index) => (
+						<Button
+							value={item}
+							key={+index}
+							color={theme.text.primary}
+							bgColor={item === selectedOption ? 'gray.100' : 'none'}
+							borderRadius="full"
+							w="20"
+							fontSize="xs"
+							fontWeight="bold"
+							h="6"
+							_hover={{}}
+							_active={{}}
+							_focus={{}}
+							onClick={() => handleSelectedOption(item)}
+						>
+							{item}
+						</Button>
+					))}
+					{/* <Button
+						color={theme.text.primary}
+						bgColor="gray.100"
+						borderRadius="full"
+						w="20"
+						fontSize="xs"
+						fontWeight="bold"
+						h="6"
+					>
+						This Month
+					</Button> */}
+				</Flex>
+			</Flex>
+			<Flex
+				h="100%"
+				w="100%"
+				justify="center"
+				color="black"
+				direction="column"
+				textAlign="center"
+				position="absolute"
+			>
+				<Text fontSize="xs">Withdrawn</Text>
+				<Text fontWeight="medium">{withdrawnPercentage}%</Text>
 			</Flex>
 		</Flex>
-		<Flex
-			h="100%"
-			w="100%"
-			justify="center"
-			color="black"
-			direction="column"
-			textAlign="center"
-			position="absolute"
-		>
-			<Text fontSize="xs">Withdrawn</Text>
-			<Text fontWeight="medium">{withdrawnPercentage}%</Text>
-		</Flex>
-	</Flex>
-);
+	);
+};
