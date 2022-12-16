78db9cf0a07e3c370c48388078ff910ed566cb56
Alexandre Morais <alexandremorais26@gmail.com>
2022-12-16 11:31:20 -0300

    fix: change route name in navigationPaths

--- pages/app/organizations/[overview].tsx
+++ pages/app/organizations/[overview].tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router';
 export const Overview = () => {
 	const router = useRouter();
 	const { overview } = router.query;
-	console.log(overview);
 	return (
 		<OrganizationsProvider>
 			<OverviewTab />
