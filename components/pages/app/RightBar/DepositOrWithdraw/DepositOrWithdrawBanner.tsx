e5705f866c8b3e85ac09801c2d83db0df538cfff
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-01-31 10:58:06 -0300

    feat: add have a problem card

--- components/pages/app/RightBar/DepositOrWithdraw/DepositOrWithdrawBanner.tsx
+++ components/pages/app/RightBar/DepositOrWithdraw/DepositOrWithdrawBanner.tsx
@@ -1,23 +1,31 @@
 import { Flex } from '@chakra-ui/react';
 import { usePicasso } from 'hooks';
-import { InfosBanner, LifeIsEasier, DepositOrWithdrawCard } from 'components';
+import {
+	InfosBanner,
+	LifeIsEasier,
+	DepositOrWithdrawCard,
+	HaveProblemCard,
+} from 'components';
 
 export const DepositOrWithdrawBanner = () => {
 	const theme = usePicasso();
 	return (
-		<InfosBanner>
-			<Flex
-				bg={theme.bg.black}
-				zIndex="docked"
-				gap="20"
-				p="4"
-				direction="column"
-				borderRadius="base"
-				className="deposit-withdraw-banner"
-			>
-				<DepositOrWithdrawCard />
-				<LifeIsEasier />
-			</Flex>
-		</InfosBanner>
+		<Flex direction="column" gap="8">
+			<InfosBanner>
+				<Flex
+					bg={theme.bg.black}
+					zIndex="docked"
+					gap="20"
+					p="4"
+					direction="column"
+					borderRadius="base"
+					className="deposit-withdraw-banner"
+				>
+					<DepositOrWithdrawCard />
+					<LifeIsEasier />
+				</Flex>
+			</InfosBanner>
+			<HaveProblemCard />
+		</Flex>
 	);
 };
