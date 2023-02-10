ab8a48c5c9e218b3cd7f59d7d92a1c796b812853
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-02-10 12:11:12 -0300

    feat: implement life is easir tablet in companies page

--- components/pages/app/RightBar/CompaniesRightBar.tsx
+++ components/pages/app/RightBar/CompaniesRightBar.tsx
@@ -1,9 +1,18 @@
 import { Flex } from '@chakra-ui/react';
-import { HaveProblemCard, LifeIsEasierBanner } from 'components';
+import {
+	HaveProblemCard,
+	LifeIsEasierBanner,
+	LifeIsEasierTabletBreakpoint,
+} from 'components';
 
 export const CompaniesRightBar = () => (
 	<Flex direction="column" gap="8">
-		<LifeIsEasierBanner />
+		<Flex display={{ md: 'none', lg: 'flex' }}>
+			<LifeIsEasierBanner />
+		</Flex>
+		<Flex display={{ md: 'flex', lg: 'none' }}>
+			<LifeIsEasierTabletBreakpoint />
+		</Flex>
 		<HaveProblemCard />
 	</Flex>
 );
