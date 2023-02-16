860042aacbf7606935f776e3f1b3bea46d244a58
Alexandre Morais <alexandremorais26@gmail.com>
2023-02-16 18:49:28 -0300

    feat: add i18n in copied address toast

--- components/Toasts/CopyAddress.tsx
+++ components/Toasts/CopyAddress.tsx
@@ -1,35 +1,39 @@
 import { Button, Flex, Icon, Text } from '@chakra-ui/react';
 import { OffsetShadow } from 'components';
+import useTranslation from 'next-translate/useTranslation';
 import { AiFillCheckCircle, AiOutlineClose } from 'react-icons/ai';
 
 interface IToast {
 	onClick: () => void;
 }
-export const CopyAddressToast: React.FC<IToast> = ({ onClick }) => (
-	<OffsetShadow
-		top="0.25rem"
-		left="0.25rem"
-		borderColor="green.500"
-		bg="green.500"
-	>
-		<Flex
-			bg="white"
-			h="14"
-			borderWidth="1px"
+export const CopyAddressToast: React.FC<IToast> = ({ onClick }) => {
+	const { t: translate } = useTranslation('companies');
+	return (
+		<OffsetShadow
+			top="0.25rem"
+			left="0.25rem"
 			borderColor="green.500"
-			borderRadius="base"
-			align="center"
-			color="gray.700"
-			gap="16"
-			px="4"
+			bg="green.500"
 		>
-			<Flex gap="3" align="center">
-				<Icon as={AiFillCheckCircle} color="green.500" boxSize="5" />
-				<Text w="max-content">Address copied successfully</Text>
+			<Flex
+				bg="white"
+				h="14"
+				borderWidth="1px"
+				borderColor="green.500"
+				borderRadius="base"
+				align="center"
+				color="gray.700"
+				gap="16"
+				px="4"
+			>
+				<Flex gap="3" align="center">
+					<Icon as={AiFillCheckCircle} color="green.500" boxSize="5" />
+					<Text w="max-content">{translate('addressCopiedSuccessfully')}</Text>
+				</Flex>
+				<Button onClick={onClick}>
+					<Icon boxSize="5" as={AiOutlineClose} />
+				</Button>
 			</Flex>
-			<Button onClick={onClick}>
-				<Icon boxSize="5" as={AiOutlineClose} />
-			</Button>
-		</Flex>
-	</OffsetShadow>
-);
+		</OffsetShadow>
+	);
+};
