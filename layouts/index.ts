5818faf479879bae21f66676ee46122a67926a95
Amaury Magalhães <amaurymagalhaesf@hotmail.com>
2023-03-02 10:49:31 -0300

    fix: typo on file

--- layouts/index.ts
+++ layouts/index.ts
@@ -2,6 +2,6 @@ export * from './AppLayout';
 export * from './CompaniesConnected';
 export * from './CompaniesNoConnected';
 export * from './CompanyWhiteBackground';
-export * from './editProfileLayout';
+export * from './EditProfileLayout';
 export * from './LandingPage';
 export * from './MobileModalLayout';
