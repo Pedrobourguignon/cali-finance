0c15afab81b0e770628c7763186f2478006a6953
Pedrobourguignon <pedrobourguignon@hotmail.com>
2023-05-29 10:48:05 -0300

    feat: add TODO

--- components/Modals/AddEmployee/AddEmployee.tsx
+++ components/Modals/AddEmployee/AddEmployee.tsx
@@ -106,6 +106,7 @@ export const AddEmployee: React.FC<IAddEmployee> = ({ isOpen, onClose }) => {
 		}));
 	};
 
+	// Todo: update address
 	const { config: addEmployeeConfig } = usePrepareContractWrite({
 		address: '0x8409809BdF2424C45Fb85DB7768daC6026e95602',
 		abi: companyAbi,
