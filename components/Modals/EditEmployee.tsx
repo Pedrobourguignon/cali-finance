3503f90536ff29767ba9017075e5f1d21f260388
Alexandre Morais <alexandremorais26@gmail.com>
2023-08-08 15:59:21 -0300

    refactor: change the flow of edit employee when employee dont exist in the company contract

--- components/Modals/EditEmployee.tsx
+++ components/Modals/EditEmployee.tsx
@@ -33,7 +33,11 @@ import { formatDecimals, getCoinLogo, mainClient, truncateWallet } from 'utils';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { AxiosError } from 'axios';
 import useTranslation from 'next-translate/useTranslation';
-import { useContractWrite, useWaitForTransaction } from 'wagmi';
+import {
+	useContractWrite,
+	usePrepareContractWrite,
+	useWaitForTransaction,
+} from 'wagmi';
 import companyAbi from 'utils/abi/company.json';
 
 export const EditEmployee: React.FC<IEditEmployee> = ({
@@ -139,15 +143,75 @@ export const EditEmployee: React.FC<IEditEmployee> = ({
 		{ enabled: false }
 	);
 
+	const { config: addEmployeeConfig } = usePrepareContractWrite({
+		address: selectedCompany.contract,
+		abi: companyAbi,
+		functionName: 'addEmployee',
+		args: [
+			employee.wallet,
+			formatDecimals(editedEmployeeData.amount, selectedCompany.tokenDecimals),
+		],
+		enabled: employee.wallet !== '' && editedEmployeeData.amount !== 0,
+	});
+
+	const { data: addEmployeeData, write: addEmployeeWrite } =
+		useContractWrite(addEmployeeConfig);
+
+	const { isLoading: useWaitForTransactionLoading } = useWaitForTransaction({
+		hash: addEmployeeData?.hash,
+		confirmations: 3,
+		onSuccess() {
+			handleResetFormInputs();
+			toast({
+				position: 'top',
+				render: () => (
+					<AlertToast
+						onClick={toast.closeAll}
+						text="employeeAdded"
+						type="success"
+					/>
+				),
+			});
+		},
+		onError() {
+			handleResetFormInputs();
+			toast({
+				position: 'top',
+				render: () => (
+					<AlertToast
+						onClick={toast.closeAll}
+						text="weAreWorkingToSolve"
+						type="error"
+					/>
+				),
+			});
+		},
+	});
+
 	const { data: editEmployeeData, write: editEmployeeWrite } = useContractWrite(
 		{
 			address: selectedCompany.contract,
 			abi: companyAbi,
 			functionName: 'updateEmployeeSalary',
+			onError(error: any) {
+				if (error.cause.data.args[0] === "Employee doesn't exists.") {
+					toast({
+						position: 'top',
+						render: () => (
+							<AlertToast
+								onClick={toast.closeAll}
+								text="employeeNotYetDeployed"
+								type="warning"
+							/>
+						),
+					});
+					addEmployeeWrite?.();
+				}
+			},
 		}
 	);
 
-	const { isLoading: useWaitForTransactionLoading } = useWaitForTransaction({
+	const { isLoading: useWaitForTransactionEdit } = useWaitForTransaction({
 		hash: editEmployeeData?.hash,
 		confirmations: 3,
 		onSuccess() {
@@ -241,7 +305,7 @@ export const EditEmployee: React.FC<IEditEmployee> = ({
 				setToken={setToken}
 			/>
 			<WaitMetamaskFinishTransaction
-				isOpen={useWaitForTransactionLoading}
+				isOpen={useWaitForTransactionLoading || useWaitForTransactionEdit}
 				onClose={onCloseLoadingConfirmation}
 			/>
 			<ModalContent
